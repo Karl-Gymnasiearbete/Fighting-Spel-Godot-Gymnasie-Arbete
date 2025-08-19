c66cfa4e61fddcdb8ac8619589a3e71914000ac2
Karl-Gymnasiearbete <karl.borrstade02@edu.vellinge.se>
2025-08-19 17:24:14 +0200

    Skrev kod, Laddade upp temporära bilder

--- Script/1ScriptCharacter.gd
+++ Script/1ScriptCharacter.gd
@@ -3,20 +3,18 @@ extends CharacterBody2D
 #Variabler
 @export var speed = 200
 @export var jump = -300
+@onready var animatedSprite = $AnimatedSprite2D
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var isInCombo = false
 
-func _physics_process(delta):
-	#Jump funktioner
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = jump
-	
-	#movement funtioner
-	var direction = Input.get_axis("Left","Right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0 , speed)
-	move_and_slide()
+var timeTillNextInput = 0.5
+var time = 0
+
+var currentAttack = 0
+var previousAttack = 0
+
+func _ready():
+	time = timeTillNextInput
+
+func _process(delta):
+	pass
