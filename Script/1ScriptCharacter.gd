13348fdc3e61421e4fb98c4875efaf81dc7c05c3
Karl-Gymnasiearbete <karl.borrstade02@edu.vellinge.se>
2025-08-19 18:32:11 +0200

    Skrev en del skit, fungerar ej

--- Script/1ScriptCharacter.gd
+++ Script/1ScriptCharacter.gd
@@ -10,11 +10,35 @@ var isInCombo = false
 var timeTillNextInput = 0.5
 var time = 0
 
-var currentAttack = 0
-var previousAttack = 0
+var current_Attack = 0
+var previous_Attack = 0
 
 func _ready():
 	time = timeTillNextInput
 
 func _process(delta):
-	pass
+	if(Input.is_action_just_pressed("ui_punch")):
+		if(current_Attack == 0):
+			animatedSprite.play("Punch")
+		elif(current_Attack == 1):
+			animatedSprite.play("Upper Cut")
+		elif(current_Attack == 2):
+			animatedSprite.play("Kick")
+		
+		isInCombo = true
+		current_Attack += 1
+		_Reset_Attack_Timer()
+	
+	if(isInCombo):
+		time -= delta
+		
+		if(time <0):
+			time = timeTillNextInput
+			isInCombo = false
+			current_Attack = 0
+			animatedSprite.play("Idle")
+
+
+func _Reset_Attack_Timer():
+	if(current_Attack < 4):
+		time = timeTillNextInput
