07f96726033901359c7e746e8ceaa34eeb093c8d
Karl-Gymnasiearbete <karl.borrstade02@edu.vellinge.se>
2025-09-08 17:41:36 +0200

    asdf

--- Player-Movement.gd
+++ Player-Movement.gd
@@ -11,12 +11,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
+<<<<<<< Updated upstream
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
+=======
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+>>>>>>> Stashed changes
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+<<<<<<< Updated upstream
 	var direction := Input.get_axis("Left", "Right")
+=======
+	var direction := Input.get_axis("ui_left", "ui_right")
+>>>>>>> Stashed changes
 	if direction:
 		velocity.x = direction * SPEED
 	else:
